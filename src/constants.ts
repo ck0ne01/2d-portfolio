7ea27c03d7db82d891001810ffaf39f3c53bdbc7
Christian Kirmse <cke@dasburo.com>
2024-07-15 17:19:18 +0200

    feat: add new dialogs

--- src/constants.ts
+++ src/constants.ts
@@ -1,20 +1,17 @@
 export const SCALE_FACTOR = 4;
 
-export const DIALOGUE_DATA = {
-  pc: `This is my Macbook. I work mostly in DevOps these days but also did a couple of JavaScript/TypeScript projects at work.
-    In my freetime I also use Golang. Anyway regardless of the language, I just like programming.
-    Here is my <a href="https://github.com/ck0ne01" target="_blank">Github</a>!`,
-  "sofa-table": `That's my sofa. I like to relax here, watch YouTube or study spanish.`,
 export const DIALOG_DATA = {
+  pc: `This is my Macbook. I work mostly in DevOps and JavaScript/TypeScript projects these days.
+    In my freetime I also use Golang and try to sneak it into work ;). Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/ck0ne01" target="_blank">Github</a>! I use neovim btw.`,
+  "sofa-table": `Sofa.sh: Loading comfort protocols... ¡Ay caramba! User caught in an infinite loop of comfy cushions and Spanish subjunctives. Escape key temporarily disabled`,
   tv: `That's my TV. I've been watching tech youtubers a lot recently like :
    <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
   <a href="https://www.youtube.com/@ByteByteGo" target="_blank">ByteByteGo</a> and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-  kitchen: `When I'm not coding, I'm cooking. I like to cook a lot. I'm always looking for new recipes. I also like to make my own`,
-  projects: `Info about this portfolio: It's made with the Kaplay.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-  <a href="https://kaplayjs.com" target="_blank">Kaplay.js</a> by visiting their website.`,
-  library: `There are a lot of programming books on my shelves and a couple of non-technical ones too. There is even one in spanish.`,
+  bed: `Error 404: Sleep not found. Developer's rest cycle interrupted by endless coding loops and caffeine-induced debugging sessions.`,
+  kitchen: `When I'm not coding, I'm cooking. I like to cook a lot. I'm always looking for new recipes. Recently I discovered <a href="https://www.chefgpt.xyz/" target="_blank">ChefGPT</a>`,
+  projects: `Info about this portfolio: It's made with the Kaplay.js library which is a library for making games in JavaScript/Typescript. Text is rendered with HTML/CSS. I got my inspiration for this project from <a href="https://www.youtube.com/watch?v=wy_fSStEgMs&list=LL&index=5" target="_blank">a great video of freeCodeCamp</a>. Learn more about how to use <a href="https://kaplayjs.com" target="_blank">Kaplay.js</a> by visiting their website, or have a look at the source code in my <a href="https://github.com/ck0ne01/2d-portfolio" target="_blank">GitHub Repo</a>`,
+  library: `There are a lot of programming books on my shelves and a couple of non-technical ones too. There is even one in spanish. Some of my favorits are <a href="https://www.goodreads.com/book/show/4099.The_Pragmatic_Programmer" target="_blank">The Pragmatic Programmer</a>, <a href="https://www.goodreads.com/book/show/23463279-designing-data-intensive-applications?from_search=true&from_srp=true&qid=PD8zjmOYx7&rank=1" target="_blank">Designing Data-Intensive Applications</a>, <a href="https://www.goodreads.com/book/show/26083308-the-devops-handbook?from_search=true&from_srp=true&qid=ETAxNI1SDF&rank=1" target="_blank">The DevOps Handbook</a>, <a href="https://www.goodreads.com/book/show/35747076-accelerate?from_search=true&from_srp=true&qid=iKicZGkC8m&rank=1" target="_blank">Accelerate</a> and <a href="https://www.goodreads.com/book/show/1194329.Cuentos_de_la_selva?from_search=true&from_srp=true&qid=dP7MpenLuL&rank=1" target="_blank">Cuentos de la selva</a>.`,
   exit: `If you want to exit my portfolio, just close the tab.`,
-  window: `This is the beautiful skyline of Berlin. This is where I live and work.`,
+  window: `This is the beautiful skyline of Berlin. This is where I live and where my current <a href="https://dasburo.com/" target="_blank">work</a>  is located.`,
 };
